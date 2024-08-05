c416942c4b9e8726aaeff76cfebb8214cc8b8e17
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2024-08-04 21:35:02 -0300

    add auth context

--- src/layout/user-layout.tsx
+++ src/layout/user-layout.tsx
@@ -1,8 +1,19 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import LogoNavbar from "../assets/logo-navbar";
 import { CustomLink } from "../components/link";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth-context";
 export function UserLayout() {
-  const handleLogout = () => alert("saiu");
+  const navigate = useNavigate();
+  const { isAuthenticated, isAdmin, signOut } = useContext(AuthContext);
+
+  if (!isAuthenticated) return navigate("/login");
+
+  if (isAdmin) return navigate("/admin");
+  const handleLogout = () => {
+    signOut();
+    return navigate("/login");
+  };
   return (
     <main className="pb-10">
       <nav className="flex justify-between px-10 py-5 bg-emerald-500 border-b border-emerald-400 text-gray-50">
--- src/pages/occurrences/page.tsx
+++ src/pages/occurrences/page.tsx
@@ -1,16 +1,7 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/auth-context";
 import { OccurencesHeader } from "./components/occurences-header";
 import { OccurencyItem } from "./components/occurency-item";
 
 export function OccurrencesPage() {
-  const navigate = useNavigate();
-  const { isAuthenticated, isAdmin } = useContext(AuthContext);
-
-  if (!isAuthenticated) return navigate("/login");
-
-  if (isAdmin) return navigate("/admin");
   return (
     <main className="flex flex-col">
       <OccurencesHeader />
